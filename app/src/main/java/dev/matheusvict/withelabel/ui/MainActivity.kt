897bfe92af0bc1d0c705d13ca3774f0af58f8f5a
MatheusVict <matheusvictorhenrique@gmail.com>
2023-08-24 20:06:00 -0300

    feat: fragment and viewBinding
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- app/src/main/java/dev/matheusvict/withelabel/ui/MainActivity.kt
+++ app/src/main/java/dev/matheusvict/withelabel/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package dev.matheusvict.withelabel
+package dev.matheusvict.withelabel.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.matheusvict.withelabel.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/dev/matheusvict/withelabel/ui/products/ProductFragment.kt
@@ -0,0 +1,24 @@
+package dev.matheusvict.withelabel.ui.products
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.matheusvict.withelabel.R
+import dev.matheusvict.withelabel.databinding.FragmentProductBinding
+
+class ProductFragment : Fragment() {
+
+    private var _binding: FragmentProductBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProductBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
