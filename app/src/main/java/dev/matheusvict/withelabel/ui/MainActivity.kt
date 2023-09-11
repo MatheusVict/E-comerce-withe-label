563331ab050c5e12481b81d63b477cb5cf66a51f
MatheusVict <matheusvictorhenrique@gmail.com>
2023-09-10 22:19:25 -0300

    feat: main activity binding
    Signed-off-by: MatheusVict <matheusvictorhenrique@gmail.com>

--- app/src/main/java/dev/matheusvict/withelabel/ui/MainActivity.kt
+++ app/src/main/java/dev/matheusvict/withelabel/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package dev.matheusvict.withelabel.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dev.matheusvict.withelabel.R
+import dev.matheusvict.withelabel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbarMain.setupWithNavController(navController, appBarConfiguration)
     }
 }
